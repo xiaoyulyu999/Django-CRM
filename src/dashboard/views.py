c4366b9c9e1903ee47e8ac386a9811effe1534cc
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-12-29 20:55:59 +0000

    work

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -6,7 +6,7 @@ def dashboard_index(request, *args, **kwargs):
     if not request.user.is_authenticated:
         return redirect("/auth/google/login/")
 
-    my_value = str(request.user) + str(randint(1, 100))
+    my_value = str(request.user) + str(randint(1, 1000000))
     template_context = {
         "my_value": my_value,
         "not_actual_context": "now it's ready",
