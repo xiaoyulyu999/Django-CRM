35ffcebf1678c7f83326e1d9bda72fea0747ce4e
xiaoyulyu999 <info@bloomora.net>
2025-12-29 13:01:48 +0000

    home

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
-def dashboard_index(request):
+def dashboard_index(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/auth/google/login/")
     return HttpResponse("Hello, world. You're at the dashboard index.")
