d813a2c0f1c76429f7623171a0fc2d10eb04157a
xiaoyulyu999 <info@bloomora.net>
2025-12-29 14:38:15 +0000

    home

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from random import randint
 
 
 def dashboard_index(request, *args, **kwargs):
     if not request.user.is_authenticated:
         return redirect("/auth/google/login/")
-    return HttpResponse(f"Hello, {request.user}. You're at the dashboard index.")
+
+    my_value = str(request.user) + str(randint(1, 100))
+    template_context = {
+        "my_value": my_value,
+        "not_actual_context": "now it's ready",
+        "colors": ["red", "blue"],
+    }
+    return render(request, "dashboard/main.html", template_context)
