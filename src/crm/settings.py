f331f4132f34a674314675826dfe95eb190f03d0
xiaoyulyu999 <info@bloomora.net>
2026-01-01 17:54:06 +0000

    home

--- src/crm/settings.py
+++ src/crm/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "mystaticfiles")
 # manage.py collectstatic
 
 STATICFILES_DIRS = [
